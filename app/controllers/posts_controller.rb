762e22827d905e8c773d9d692aa390c3abd6902e
George  Pianka <gpianka1@gmail.com>
2019-02-16 08:11:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @authors = Author.all
     if !params[:author].blank?
-    @posts = Post.by_author(params[:author]
+    @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
       if params[:date] == "Today"
         @posts = Post.from_today
